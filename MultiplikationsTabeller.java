16661952778d12e623bb7b7735bc80b6f39cafd2
Avebave <arvid.hedstrom@elev.ga.ntig.se>
2023-10-23 14:33:38 +0200

    Uppgifter

--- MultiplikationsTabeller.java
+++ MultiplikationsTabeller.java
@@ -2,11 +2,30 @@ package Ugggifter;
 import javax.swing.JOptionPane;
 import java.util.Scanner;
 public class MultiplikationsTabeller {
-    public static void While(String[] args) {
+
+    public static void main(String[] args) {
         System.out.println("Vilken gånger tabell?");
         Scanner tgb = new Scanner(System.in);
         int Tabell = Integer.parseInt(tgb.nextLine());
 
-        While();
+        int x = 1;
+
+        /*while (x <= 10) {
+            System.out.println(x*Tabell);
+            x++;
+
+        }*/
+
+        /*for (x=1 ; x <= 10 ; x++) {
+            System.out.println(x*Tabell);
+        }*/
+
+        do {
+            System.out.println(x*Tabell);
+            x++;
+        } while (x <= 10);
+
+
+
     }
 }
