5d95b39abc83cfff921c486987317e9bf779f4f4
Avebave <arvid.hedstrom@elev.ga.ntig.se>
2024-03-15 16:48:09 +0100

    Uppgifter

--- Sid269.java
+++ Sid269.java
@@ -1,22 +1,67 @@
 package Ugggifter;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sid269 {
     public static void main(String[] args) {
-        System.out.println("Vilken uppgift?");
+        System.out.println("Vilken uppgift?");//brukar ha en sådan här för att kunna ha alla uppgifter i samma kod
         Scanner tgb = new Scanner(System.in);
         int uppg = Integer.parseInt((tgb.nextLine()));
         if (uppg == 1) {
             uppg1();
         }
+
     }
 
-    public static String uppg1(String[] args) {
-        int [] Nummer = new int [1000];
+    public static int[] uppg1() {
+        int [] Nummer = new int[1000];
+        int i = 0;
+        System.out.println("nummrerna tack");
+        Scanner Scanner = new Scanner(System.in);
+        String NummerStr = null;
+        String NummerPrint = null;
+        int loop = 1; //hade problem med att sista talet inskrivet saknades så gjorde en loop för att den inte ska sluta direkt då ctrl+d trycks
+            while (loop!=0){
+                if (Scanner.hasNextInt()){//för att loopen inte ska ta slut tidigt
+                    loop ++;
+                }
+
+                boolean add = true; //ser till att rätt tal läggs till
+                int NummerCheck = 0;
+                int[] Nummermid = Arrays.copyOfRange(Nummer, 0, i); //ser till att rätt nummer kopieras
+                NummerStr = Arrays.toString(Nummermid); //gör om det till string så det går lätt att jämföra mot nya tal och printas ut
+
+                if (Scanner.hasNextInt()){
+                    NummerCheck = Integer.parseInt(Scanner.nextLine());
+                }
+
+                if (NummerStr.contains("" + NummerCheck)) { //kollar om nummret finns redan
+                    add = false;
+                }
+
+                if (add == true){ //lägger till nummer
+                    Nummer[i] = NummerCheck;
+                    i++;
+                }
+                NummerPrint = NummerStr;
+                loop--; //avslutar loopen när ctrl+d trycks.
+            }
+
+
+
+
+
+        System.out.println(NummerPrint);
+        return Nummer;
+    }
+
+    public static String uppg2; {
+        String temp [];
         int i = 0;
 
-        while ()
-        return null;
+        System.out.println("tempen tack");
+        Scanner tgb = new Scanner(System.in);
+
     }
 }
---
+++ sid244.java
@@ -0,0 +1,21 @@
+package Ugggifter;
+
+public class sid244 {
+    public static void main(String[] args) {
+        double[]x = new double[4];
+
+        int[] y = new int[7];
+        y[0] = 5;
+        y[1] = 10;
+        y[2] = 20;
+        y[3] = 50;
+        y[4] = 100;
+        y[5] = 200;
+        y[6] = 500;
+
+        for (int i=0; i <= 6; i++){
+            System.out.println(y[i]);
+        }
+
+    }
+}
